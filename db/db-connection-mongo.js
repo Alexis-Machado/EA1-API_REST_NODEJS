9ac3656614ac1af38fad9a4294fa0d9f1c8bd537
Alex <jmachadoa12@gmail.com>
2023-03-03 16:43:33 -0500

    Primer Commit

---
+++ db/db-connection-mongo.js
@@ -0,0 +1,19 @@
+const mongo = require('mongoose');
+
+const getConnection = async() => {
+
+    try{
+        const url = 'mongodb+srv://Alexis:root@cluster0.l88s1qz.mongodb.net/?retryWrites=true&w=majority';
+
+        await mongo.connect(url);
+        console.log('Conexion Exitosa');
+
+    }catch (error){
+        console.log('Error');
+    }
+
+}
+
+module.exports={
+    getConnection,
+}
